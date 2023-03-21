8a31efbd439a4906cc0672c71c71ee880e1eb52f
Raiymbek Merekeyev <raiymbekmerekeyev@Raiymbeks-MacBook-Pro.local>
2023-03-21 19:41:14 +0600

    Fixed navbar issue with overlay

--- RestaurantBooking/Core/FoodView/Views/FoodView.swift
+++ RestaurantBooking/Core/FoodView/Views/FoodView.swift
@@ -9,36 +9,33 @@ import SwiftUI
 
 struct FoodView: View {
     @StateObject private var foodVM = FoodViewModel()
+    @State private var offset: CGPoint = .zero
     let columns = [
         GridItem(.adaptive(minimum: 150), spacing: 10),
         GridItem(.adaptive(minimum: 150), spacing: 10),
     ]
+    let title: String
     var body: some View {
         ScrollView {
             LazyVStack(pinnedViews: [.sectionHeaders]){
-                Section(header: tabBarView) {
-                    LazyVGrid(columns: columns) {
-                        ForEach(foodVM.foods) { food in
-                            FoodCardView(food: food)
-                                .padding(10)
-                        }
-                    }
-                    .padding()
-                }
+                contentView
             }
         }
+        .overlay(titleBackground, alignment: .top)
+        .navigationBarBackButtonHidden(true)
     }
 }
 
 struct FoodView_Previews: PreviewProvider {
     static var previews: some View {
-        FoodView()
+        FoodView(title: "Restaurant")
     }
 }
 
 extension FoodView{
     private var tabBarView: some View{
         ScrollViewReader { scrollView in
+            navBar
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 0){
                     ForEach(foodVM.tabBars.indices, id: \.self){ index in
@@ -55,8 +52,43 @@ extension FoodView{
                 }
             }
         }
-        .background(Color.theme.background
+        .background(Color.theme.background)
+    }
+    
+    private var contentView: some View{
+        Section(header: tabBarView) {
+            LazyVGrid(columns: columns) {
+                ForEach(foodVM.foods) { food in
+                    FoodCardView(food: food)
+                        .padding(10)
+                }
+            }
+            .padding()
+        }
+    }
+    
+    private var titleBackground: some View{
+        Color.theme.background
             .frame(height: foodVM.topSafeAreaInset)
-            .ignoresSafeArea(.all, edges: .top))
+            .ignoresSafeArea(.all, edges: .top)
+    }
+    private var navBar: some View{
+        HStack{
+            Group{
+                HStack{
+                    Button{
+                        
+                    }label: {
+                        Image(systemName: "arrow.left")
+                            .padding(.horizontal)
+                    }
+                    Spacer()
+                }
+                Text(title)
+                    .font(.headline)
+                Spacer()
+            }
+            .frame(width: UIScreen.main.bounds.width * 0.33)
+        }
     }
 }
