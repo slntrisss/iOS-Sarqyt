eab6ac0936814de19030c8cf0b0b1f8bde78e362
Raiymbek Merekeyev <raiymbekmerekeyev@Raiymbeks-MacBook-Pro.local>
2023-04-02 14:03:59 +0600

    Fixed lists in profile view navigation

--- RestaurantBooking/Core/Profile/Main/View/ProfileView.swift
+++ RestaurantBooking/Core/Profile/Main/View/ProfileView.swift
@@ -11,44 +11,46 @@ import UIKit
 struct ProfileView: View {
     @StateObject private var profileVM = ProfileViewModel()
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            ProfileImageView(image: profileVM.profileImage)
-                .padding(.vertical)
-            Text(profileVM.name)
-                .font(.title2.weight(.semibold))
-            Text(profileVM.email)
-                .font(.subheadline.weight(.medium))
-            Divider()
-            VStack(spacing: 30){
-                createNavLinkField(iconName: "person", text: "Edit Profile", fontColor: Color.theme.accent)
-                    .onTapGesture {
-                        profileVM.navigateToEditProfileView = true
-                    }
-                createNavLinkField(iconName: "creditcard", text: "Payment", fontColor: Color.theme.accent)
-                    .onTapGesture {
-                        profileVM.navigateToPaymentView = true
-                    }
-                createNavLinkField(iconName: "bell", text: "Notifications", fontColor: Color.theme.accent)
-                    .onTapGesture {
-                        profileVM.openSettings()
-                    }
-                createNavLinkField(iconName: "lock.shield", text: "Security", fontColor: Color.theme.accent)
-                    .onTapGesture {
-                        profileVM.navigateToSecurityView = true
-                    }
+        NavigationStack{
+            ScrollView(.vertical, showsIndicators: false) {
+                ProfileImageView(image: profileVM.profileImage)
+                    .padding(.vertical)
+                Text(profileVM.name)
+                    .font(.title2.weight(.semibold))
+                Text(profileVM.email)
+                    .font(.subheadline.weight(.medium))
+                Divider()
+                VStack(spacing: 30){
+                    createNavLinkField(iconName: "person", text: "Edit Profile", fontColor: Color.theme.accent)
+                        .onTapGesture {
+                            profileVM.navigateToEditProfileView = true
+                        }
+                    createNavLinkField(iconName: "creditcard", text: "Payment", fontColor: Color.theme.accent)
+                        .onTapGesture {
+                            profileVM.navigateToPaymentView = true
+                        }
+                    createNavLinkField(iconName: "bell", text: "Notifications", fontColor: Color.theme.accent)
+                        .onTapGesture {
+                            profileVM.openSettings()
+                        }
+                    createNavLinkField(iconName: "lock.shield", text: "Security", fontColor: Color.theme.accent)
+                        .onTapGesture {
+                            profileVM.navigateToSecurityView = true
+                        }
+                }
+                .padding([.horizontal, .vertical])
             }
-            .padding([.horizontal, .vertical])
+            .navigationDestination(isPresented: $profileVM.navigateToEditProfileView, destination: {
+                EditProfileView()
+            })
+            .navigationDestination(isPresented: $profileVM.navigateToPaymentView, destination: {
+                PaymentView()
+            })
+            .navigationDestination(isPresented: $profileVM.navigateToSecurityView, destination: {
+                SecurityView()
+            })
+            .safeAreaInset(edge: .bottom) { logoutButton }
         }
-        .navigationDestination(isPresented: $profileVM.navigateToEditProfileView, destination: {
-            EditProfileView()
-        })
-        .navigationDestination(isPresented: $profileVM.navigateToPaymentView, destination: {
-            PaymentView()
-        })
-        .navigationDestination(isPresented: $profileVM.navigateToSecurityView, destination: {
-            SecurityView()
-        })
-        .safeAreaInset(edge: .bottom) { logoutButton }
     }
 }
 
@@ -68,6 +70,7 @@ extension ProfileView{
             Spacer()
             Image(systemName: "chevron.right")
         }
+        .contentShape(Rectangle())
         .font(.headline)
         .foregroundColor(fontColor)
     }
